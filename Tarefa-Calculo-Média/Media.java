7496056c5733d8306261713ddbb7f3d514a0b7fa
Davi França de Melo <113212827+DaviFmelo@users.noreply.github.com>
2022-09-17 18:32:26 -0300

    Media

---
+++ Tarefa-Calculo-Média/Media.java
@@ -0,0 +1,13 @@
+public class Media {
+     public static void main(String[] args){
+            System.out.println("********Media das 4 notas*********");
+
+            double media1 = 6.3;
+            double media2 = 6.2;
+            double media3 = 6.4;
+            double media4 = 5.3;
+            double mediaTotal = media1 + media2 + media3 + media4;
+
+            System.out.println("O resultado é = " +  mediaTotal / 4);
+        }
+    }
