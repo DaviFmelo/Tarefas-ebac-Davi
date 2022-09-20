80bcdaa0c93a806353ffce72447404435bc5ba98
Davi França de Melo <113212827+DaviFmelo@users.noreply.github.com>
2022-09-20 19:13:03 -0300

    Wrapper_Casting

---
+++ Tarefa_Wrapper/Wrapper_Casting.java
@@ -0,0 +1,21 @@
+public class Wrapper_Casting {
+    public static void main(String[] args){
+
+            // Wrapper para Primitivo:
+
+            int num9 = Integer.valueOf(123456);
+            System.out.println(num9);
+
+            // Primitivo para Wrapper
+
+            Integer num10 = 1;
+            System.out.println(num10);
+
+            // Casting
+
+            int num1 = 1021212;
+            System.out.println(num1);
+            short num2 = (short) num1;
+            System.out.println(num2);
+        }
+    }
