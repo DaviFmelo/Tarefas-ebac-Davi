38618955deb47b245306d4c6d1dde2e5212f66d0
Davi França de Melo <113212827+DaviFmelo@users.noreply.github.com>
2022-10-12 14:54:46 -0300

    Cadastro de Cliente

---
+++ Projeto-1/dao/IClienteDAO.java
@@ -0,0 +1,18 @@
+package Davi.dao;
+
+import Davi.domain.Cliente;
+
+import java.util.Collection;
+
+public interface IClienteDAO {
+
+    public boolean cadastrar (Cliente cliente);
+
+    public Cliente excluir (Long cpf);
+
+    public void alterar (Cliente cliente);
+
+    public Cliente consultar (Long cpf);
+
+    public Collection<Cliente> buscarTodos();
+}
